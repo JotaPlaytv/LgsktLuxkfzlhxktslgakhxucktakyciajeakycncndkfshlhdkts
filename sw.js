19c6744e0be9e6da32c950f52b4044f54ca8f2a8
JotaPlaytv <jota51074@gmail.com>
2025-11-30 17:57:00 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,28 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = 'iptv-app-v1';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  // se tiver outros arquivos (css, imagens) adicione aqui
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  console.log("Service Worker ativo");
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
 });
